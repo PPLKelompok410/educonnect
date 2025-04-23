3407494e2587ff9ad647499313080d3ef71bc2c4
Raficae <rafi.caesario555@gmail.com>
2025-04-23 19:35:09 +0700

    EDC-8
    [EDC-8] Rafi | Membuat fitur crud catatan

--- project-educonnect/app/Http/Controllers/NoteController.php
+++ project-educonnect/app/Http/Controllers/NoteController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Note;
-use Illuminate\Http\Request;
 use App\Models\MataKuliah;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class NoteController extends Controller
 {
@@ -12,15 +13,73 @@ class NoteController extends Controller
     {
         $notes = Note::with('user')
                     ->where('matkul_id', $matkul->id)
+                    ->where('type', 'galeri') 
                     ->latest()
                     ->get();
 
         return view('notes.index', compact('notes', 'matkul'));
     }
 
+    public function create(MataKuliah $matkul)
+    {
+        return view('notes.create', compact('matkul'));
+    }
+
+    public function store(Request $request, MataKuliah $matkul)
+    {
+        $request->validate([
+            'judul' => 'required|string|max:255',
+            'file' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        $filePath = $request->file('file')->store('catatan', 'public');
+
+        Note::create([
+            'user_id' => 1,
+            'judul' => $request->judul,
+            'file_path' => $filePath,
+            'matkul_id' => $matkul->id,
+            'type' => 'galeri',
+        ]);
+
+        return redirect()->route('notes.index', $matkul)->with('success', 'Gambar berhasil ditambahkan.');
+    }
+
     public function show($id)
     {
         $note = Note::findOrFail($id);  
         return view('notes.show', compact('note')); 
+    }
+
+    public function edit(Note $note)
+    {
+        return view('notes.edit', compact('note'));
+    }
+
+    public function update(Request $request, Note $note)
+    {
+        $request->validate([
+            'judul' => 'required|string|max:255',
+            'file' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        if ($request->hasFile('file')) {
+            Storage::disk('public')->delete($note->file_path);
+            $filePath = $request->file('file')->store('catatan', 'public');
+            $note->file_path = $filePath;
+        }
+
+        $note->judul = $request->judul;
+        $note->save();
+
+        return redirect()->route('notes.index', $note->matkul_id)->with('success', 'Gambar berhasil diperbarui.');
+    }
+
+    public function destroy(Note $note)
+    {
+        Storage::disk('public')->delete($note->file_path);
+        $note->delete();
+
+        return back()->with('success', 'Gambar berhasil dihapus.');
     }
 }
--- project-educonnect/routes/web.php
+++ project-educonnect/routes/web.php
@@ -15,6 +15,7 @@ Route::get('/matkul/{id}/edit', [MataKuliahController::class, 'edit'])->name('ma
 Route::put('/matkul/{id}', [MataKuliahController::class, 'update'])->name('matkul.update');
 Route::delete('/matkul/{id}', [MataKuliahController::class, 'destroy'])->name('matkul.destroy');
 
+
 // Page Diskusi di Matkul
 Route::get('/matkul/{id}/discussion', [MataKuliahController::class, 'discussion'])->name('matkul.discussion');
 Route::post('/matkul/{matkulId}/comments', [CommentController::class, 'store'])->name('comments.store');
@@ -24,6 +25,13 @@ Route::delete('/comments/{id}', [CommentController::class, 'destroy'])->name('co
 
 // Page notes 
 Route::get('/matkul/{matkul}', [NoteController::class, 'index'])->name('notes.index');
+Route::get('/notes/create/{matkul}', [NoteController::class, 'create'])->name('notes.create');
+Route::post('/notes/{matkul}', [NoteController::class, 'store'])->name('notes.store');
+Route::get('/notes/{note}', [NoteController::class, 'show'])->name('notes.show');
+Route::get('/notes/{note}/edit', [NoteController::class, 'edit'])->name('notes.edit');
+Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
+Route::delete('/notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
+
 
 // Page comments di notes
 Route::get('/notes/{note}', [NoteController::class, 'show'])->name('notes.show');
