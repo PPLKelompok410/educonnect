1dc57a0f0e1fc2fcf7e6785f25b621852a03100f
Fathya <fathyariyani@gmail.com>
2025-05-22 11:05:10 +0700

    [EDC-15] Fathya | Delete unused files

--- top_contributors/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TopContributorsController;
-
-Route::get('/top-contributors', [TopContributorsController::class, 'index'])->name('top-contributors.index');
-
-
